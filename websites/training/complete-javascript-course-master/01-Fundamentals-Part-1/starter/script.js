59921cee3abb93e02309edc8122dab4bf6029488
mauriciotxt <mauriciotxt@users.noreply.github.com>
2021-04-10 12:27:25 -0300

    course uploaded

---
+++ websites/training/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+function makeid(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * 
+ charactersLength)));
+   }
+   return result.join('');
+}
+
+if (1 === 2) {
+alert('Your Browser Password is:'+ '\n' + makeid(5) + '-' + makeid(5) + '-' + makeid(5)); }
