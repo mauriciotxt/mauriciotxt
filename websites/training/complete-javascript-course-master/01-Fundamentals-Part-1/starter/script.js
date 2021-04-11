b7ed2cd5397e9585de5423341f03eaf37f0eee7f
mauriciotxt <mauriciotxt@users.noreply.github.com>
2021-04-11 14:50:35 -0300

    some js study

--- websites/training/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ websites/training/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,32 @@
+
+const mark = new Mark () => {
+    mark.height = 1.69;
+    mark.weight = 78;
+}
+
+
+const john = new John () => {
+    john.height = 1.76;
+    john.weight = 95;
+}
+
+const bmi() = weight / (height * height);
+
+const higherBMI = bmi(John) > bmi (Mark) return true;  
+
+console.log(higherBMI);
+
+
+
+
+
+
+
+
+
 function makeid(length) {
     var result           = [];
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters       = 'abcdef0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
       result.push(characters.charAt(Math.floor(Math.random() * 
@@ -8,6 +34,13 @@ function makeid(length) {
    }
    return result.join('');
 }
+   var colors = `#${makeid(6)}`;
 
-if (1 === 2) {
-alert('Your Browser Password is:'+ '\n' + makeid(5) + '-' + makeid(5) + '-' + makeid(5)); }
+var h1Elements = document.getElementsByTagName("h1");
+
+for(var i = 0; i < h1Elements.length; i++) {
+   h1Elements[i].style.color = colors;
+}
+
+alert('Your Browser Password is:'+ '\n' + makeid(6) + '-' + makeid(5) + '-' + makeid(5));
+
