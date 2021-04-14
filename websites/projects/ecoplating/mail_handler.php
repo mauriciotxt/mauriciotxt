a4c8684108e8bd71fec42cb86f7c9778f82c7f19
mauriciotxt <mauriciotxt@users.noreply.github.com>
2021-04-14 16:59:33 -0300

    website php changes

--- websites/projects/ecoplating/mail_handler.php
+++ websites/projects/ecoplating/mail_handler.php
@@ -3,18 +3,18 @@
 		$name=$_POST['name'];
 		$email=$_POST['email'];
 		$phone=$_POST['phone'];
-		$msg=$_POST['msg'];
+		$msg=$_POST['comments'];
 
 		$to='xyz.123@mail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Form Submission';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$comments;
 		$headers="From: ".$email;
 
 		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+			echo "<h1>Enviado com Sucesso! Obrigado!"." ".$name.", Entraremos em contato em breve!</h1>";
 		}
 		else{
-			echo "Something went wrong!";
+			echo "Algo deu errado! Tente novamente.";
 		}
 	}
 ?>
