7659ce4ac7a5c48d00a5cf4892d566c8532a6172
mauriciotxt <mauriciotxt@users.noreply.github.com>
2021-05-11 10:39:54 -0300

    my first day project ( expanding cards)

---
+++ websites/projects/my_50_projects/1_Expanding_Cards/script.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active')
+    })
+});
+
+panels.forEach((panel) => {
+    panel.addEventListener('dblclick', () => {
+        removeActiveClasses();
+    })
+});
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+    })
+};
