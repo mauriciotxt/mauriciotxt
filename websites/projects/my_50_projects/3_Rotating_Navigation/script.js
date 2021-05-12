48a2fe50e7828c6c20e14d469c796f8962a56687
mauriciotxt <mauriciotxt@users.noreply.github.com>
2021-05-11 22:24:57 -0300

    pain function

--- websites/projects/my_50_projects/3_Rotating_Navigation/script.js
+++ websites/projects/my_50_projects/3_Rotating_Navigation/script.js
@@ -1,18 +1,23 @@
 const open = document.getElementById ('open')
 const close = document.getElementById ('close')
 const container = document.querySelector ('.container')
-const content = document.querySelector ('.content')
 
 
 open.addEventListener('click', () => 
 
-{container.classList.add ('show-nav') 
-content.classList.add ('show-img')
+{container.classList.add ('show-nav')  
+addPain()
 })
-
 
 close.addEventListener('click', () => 
 {container.classList.remove ('show-nav')
-content.classList.remove ('show-img')
-
+removePain()
 })
+
+function addPain() {
+    document.getElementById("img1").src = "https://64.media.tumblr.com/tumblr_lpw3yiCQjI1qdqlhzo1_500.gifv";
+  }
+
+  function removePain() {
+    document.getElementById("img1").src = "https://i.pinimg.com/originals/1b/69/d2/1b69d2dd50ff993833db64bd7c84aa2d.gif";
+  }
