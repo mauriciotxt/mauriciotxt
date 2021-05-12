f1ac6509455fc5bd624f67df317b2c0985f7c779
mauriciotxt <mauriciotxt@users.noreply.github.com>
2021-05-11 21:25:05 -0300

    remove img class

--- websites/projects/my_50_projects/3_Rotating_Navigation/script.js
+++ websites/projects/my_50_projects/3_Rotating_Navigation/script.js
@@ -1,3 +1,18 @@
-const open = document.getElementById ('open');
-const close = document.getElementById ('close');
-const container = document.querySelector ('.container');
+const open = document.getElementById ('open')
+const close = document.getElementById ('close')
+const container = document.querySelector ('.container')
+const content = document.querySelector ('.content')
+
+
+open.addEventListener('click', () => 
+
+{container.classList.add ('show-nav') 
+content.classList.add ('show-img')
+})
+
+
+close.addEventListener('click', () => 
+{container.classList.remove ('show-nav')
+content.classList.remove ('show-img')
+
+})
